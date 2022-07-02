75b404dbe93ea40ac8b3d22b49801f111860f07a
Keith Frazier <keithers98@gmail.com>
2022-07-01 18:16:36 -0700

    update getOceanPaths script

--- public/src/pathfinder/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Pathfinder = void 0;
-var Pathfinder_1 = require("./Pathfinder");
-exports.Pathfinder = Pathfinder_1.default;
-//# sourceMappingURL=index.js.map
