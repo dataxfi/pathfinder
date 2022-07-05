6b0091b13efb7b1a484e962dfe246b0905588500
Keith Frazier <keithers98@gmail.com>
2022-07-04 21:54:24 -0700

    import instead of fs

--- api/storage/v2/controller.ts
+++ api/storage/v2/controller.ts
@@ -1,15 +1,14 @@
 import { asyncErrorBoundary } from "../../../src/errors";
 import { checkParams } from "../../util";
 import * as fs from "fs";
-import path from 'path'
-
+import pathsFromOcean from './chain137/pathsFromOcean.json'
+import pathsToOcean from './chain137/pathsToOcean.json'
+import oceanAddresses from './chain137/refetch.json'
 export const post = asyncErrorBoundary(async (req, res) => {
   const { chainId, tokenIn, tokenOut } = req.body;
   checkParams(chainId, tokenIn, tokenOut);
   let pathData = null;
 
-  const pathsToOcean = JSON.parse(fs.readFileSync(`public/api/storage/v2/chain${chainId}/pathsFromOcean.json`).toString());
-  const pathsFromOcean = JSON.parse(fs.readFileSync(`public/api/storage/v2/chain${chainId}/pathsToOcean.json`).toString());
   const oceanAddresses = JSON.parse(fs.readFileSync(`public/api/storage/v2/oceanAddresses.json`).toString());
 
 
