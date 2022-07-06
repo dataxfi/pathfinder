25c145e7bb9ed182142a6d9b76b20ebde25c7086
Keith Frazier <keithers98@gmail.com>
2022-07-05 19:17:32 -0700

    removed json from abi;

--- index.ts
+++ index.ts
@@ -2,13 +2,13 @@ import * as express from "express";
 import * as cors from "cors";
 import { v2_pathfinder, v2_storage } from "./api";
 import { notFound, errorHandler } from "./src/errors";
+import * as path from 'path'
 
 const app = express();
 
-app.use(express.static('public'))
-
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public', "storage", "chain137")))
 
 app.use("/api/pathfinder/v2", v2_pathfinder);
 app.use("/api/storage/v2", v2_storage)
